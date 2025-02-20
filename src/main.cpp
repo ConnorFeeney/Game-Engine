fc26a0f53957347e78dc703f1e20546872269ea0
ConnorFeeney <connor.w.feeney@gmail.com>
2025-02-20 11:13:51 -0500

    OpenGL works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv){
-    std::cout << "Hello World!";
-    return 0;
+    if(!glfwInit()){
+        std::cout << "ERR::GLFW::FAILED_TO_INIT\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGL();
+    if(version == 0){
+        std::cout << "ERR::GLAD::FAILED_TO_LOAD\n";
+        return -1;
+    }
+
+    glClearColor(0, 0, 0, 0);
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
